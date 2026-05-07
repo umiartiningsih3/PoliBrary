392ca4a36c224ae9d5fbe5e6795f5524aaa3fdea
umiarti_ningsih <umiarti.ningsih3@gmail.com>
2026-05-07 21:14:56 +0700

    add bukucontroller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -78,7 +78,6 @@
         </div>
     </section>
 
-    <!-- Popup -->
     <x-popup-informasi />
 
 </div>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\PeminjamanController;
+use App\Http\Controllers\BukuController;
 
 // Route Home[cite: 4]
 Route::get('/', [HomeController::class, 'index'])->name('home');
